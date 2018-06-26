e3b23d09f35af35a10473f40e3f82d11526e0a3d
Jesse McReady <jessemcready@gmail.com>
2018-06-26 22:34:21 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
+  new_hash = { 
+    language: {
+      type: "",
+      style: []
+    }
+  }
   # your code here
+  languages.each do |paradigm, data|
+    data.each do |language, type|
+      new_hash[language] = {
+        type: type[:type]
+      }
+      new_hash[:language][:style] << paradigm
+    end
+  end
+  new_hash
 end
